df98456f0b05a0a5dbe2e3347389640b9c6d8c77
Harshath <harshathsundaram@gmail.com>
2026-04-01 23:04:24 +0530

    Refactor practice exercises for Ruby loops to enhance readability and structure

--- fundamentals/loops/practice_exercises.rb
+++ fundamentals/loops/practice_exercises.rb
@@ -3,3 +3,27 @@ numbers = [10,20,30,40,50]
 numbers.each do |number|
   puts number
 end
+
+i=0
+while i < numbers.length
+  puts numbers[i]
+  i += 1
+end
+
+for i in numbers
+  puts i
+end
+
+numbers.length.times do |i|
+  puts numbers[i]
+end
+
+i = 0
+while i < numbers.length
+  if numbers[i] == 30
+    i+=1
+    next
+  end
+  puts numbers[i]
+  i += 1
+end
