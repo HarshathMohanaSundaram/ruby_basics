a7c305b3daa3b867b975cd99570ccb24e5ac6f1a
Harshath <harshathsundaram@gmail.com>
2026-04-08 17:41:58 +0530

    Refactor my_map and around methods to enhance functionality and demonstrate usage

--- fundamentals/methods/block.rb
+++ fundamentals/methods/block.rb
@@ -10,3 +10,25 @@ end
 my_each([1,2,3]) do |num|
   puts num
 end
+
+def my_map(array)
+  result = []
+  my_each(array) do |num|
+    result << yield(num)
+  end
+  result
+end
+
+a = my_map([1,2,3]) { |num| num * 2 }
+puts a
+
+
+def around
+  puts "Before"
+  yield
+  puts "After"
+end
+
+around do
+  puts "Doing Work"
+end
